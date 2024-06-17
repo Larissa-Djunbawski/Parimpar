35c120fb00f633885b2967f3c6f22d70b99b19ca
Larissa Djunbawski <larydjm@gmail.com>
2024-06-17 11:27:17 -0300

    feat: codigo

--- inde.js
+++ inde.js
@@ -1,6 +1,15 @@
-let  pares = [2,4]
-let impares = [3,5]
+let numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let pares = [];
+let impares = [];
 
-let comSpread = [...pares, ...impares]
+for (let i = 0; i < numeros.length; i++) {
+  if (numeros[i] % 2 === 0) {
+    pares.push(numeros[i]); // Adiciona o número par ao array pares
+  } else {
+    impares.push(numeros[i]); // Adiciona o número ímpar ao array impares
+  }
+}
 
-console.log(comSpread)
+let comSpread = [...pares, ...impares];
+
+console.log(comSpread);
